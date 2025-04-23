3ef50997c956870aa4051c9ef5099ccf336f6df1
Eduardo Ceretta <duduceretta@gmail.com>
2025-04-23 17:13:51 -0300

    update ex115

--- Exercicios/ex115/main.py
+++ Exercicios/ex115/main.py
@@ -1,6 +1,7 @@
 #Crie um pequeno sistema modularizado que permita cadastrar pessoas pelo seu nome e idade em um arquivo de texto simples
 #O sistema so vai ter duas opcoes: cadastrar uma nova pessoa e listar todas as pessoas cadastradas
 
+from time import sleep
 from utilidades.menu import menu_principal, mensagem_despedida
 from utilidades.arquivo import criar_arquivo, cadastrar_pessoa_arquivo, ver_pessoas_cadastradas
 
@@ -19,3 +20,4 @@ while True:
         break    
     else:
         print(f'Opcao {opcao} invalida')
+        sleep(1)
